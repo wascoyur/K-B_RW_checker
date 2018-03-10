0085fa8bd9b37815ff1d152f547e41403533e30b
wascoyur <mytem20007@gmail.com>
2018-03-10 18:01:50 +0500

    Переделана загрузка из файловой системы, запрещена смена ориентации экрана.

--- app/src/main/java/com/example/yur/rw_checker/DataFinder.java
+++ app/src/main/java/com/example/yur/rw_checker/DataFinder.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -16,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,12 +27,14 @@ public class DataFinder extends Activity implements View.OnClickListener{
 
     EditText etMyShoplistInbox, etMailList;
     Button btnProcessMyShopInbox, btnProcessMailList;
+    TextView tvShops, tvMaillist;
     StringBuilder sb;
     HashSet<Integer> rowMyShop;
     HashSet<Integer> rowMailList;
     HashSet<Integer> tmp;
     ArrayList<Integer> myShopList;
     ArrayList<Integer> mailList;
+    String reGex = "(\"(\\\\b)(\\\\d{1,4})((\\\\p{Punct})|(\\\\s))\")";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +43,8 @@ public class DataFinder extends Activity implements View.OnClickListener{
         etMailList = findViewById(R.id.mailText);
         btnProcessMyShopInbox = findViewById(R.id.btnLoadMyShops);
         btnProcessMailList = findViewById(R.id.btnLoadMailList);
+        tvShops = findViewById(R.id.tvShops);
+        tvMaillist = findViewById(R.id.tvMailList);
         rowMyShop = new HashSet<>();
         rowMailList = new HashSet<>();
         tmp = new HashSet<>();
@@ -55,13 +61,12 @@ public class DataFinder extends Activity implements View.OnClickListener{
         }
 //////////tmp
         s = s + ",";
-        Pattern pattern = Pattern.compile("(,|(\\r\\n)|(\\s))(\\d{1,4})(,|(\\r\\n))|(\\s)");
+        Pattern pattern = Pattern.compile("(\\b)(\\d{1,4})((\\p{Punct})|(\\s))");
         Matcher matcher = pattern.matcher(s);
         while (matcher.find()) {
             try {
-//                s = s.replaceAll("\\D", "");
-                String tmp = s.substring(matcher.start(), matcher.end());
-//                tmp.replaceAll(",", "");
+                String tmp = matcher.group();
+                tmp = tmp.replaceAll("\\D", "");
                 ls.add(Integer.parseInt(tmp));
             } catch (Exception e) {
                 e.printStackTrace();
@@ -78,10 +83,19 @@ public class DataFinder extends Activity implements View.OnClickListener{
             case R.id.btnLoadMyShops:
                 myShopList = loadList(etMyShoplistInbox, rowMyShop);
                 MessageToUser("Your Shops is loaded");
+                Collections.sort(myShopList);
+                tvShops.setText(myShopList.toString());
+                int tmp = myShopList.size();
+                btnProcessMyShopInbox.setText(String.valueOf(tmp));
+                etMyShoplistInbox.setText(null);
                 break;
             case R.id.btnLoadMailList:
                 mailList = loadList(etMailList,  rowMailList);
                 MessageToUser("List from mail is loaded");
+                Collections.sort(mailList);
+                btnProcessMailList.setText(String.valueOf(mailList.size()));
+                tvMaillist.setText(String.valueOf(mailList.toString()));
+                etMailList.setText(null);
                 break;
         }
     }
@@ -103,8 +117,8 @@ public class DataFinder extends Activity implements View.OnClickListener{
         ArrayList<String> s = new ArrayList<>();
         String sd = android.os.Environment.getExternalStorageState();
         File sdDir = android.os.Environment.getExternalStorageDirectory();
-        File filePath = new File(sdDir.getAbsolutePath() + "/download/");
-        File sdFile = new File(filePath, "InboxMail.txt");
+        File filePath = new File(sdDir.getAbsolutePath());
+        File sdFile = new File(filePath, "1111.txt");
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(sdFile));
             String t ;
